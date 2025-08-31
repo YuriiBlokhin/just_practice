8d729c852a219b7a557a7ce6d4938ec61a3fe4e2
blokhin.y <barhatomtrav@gmail.com>
2025-08-31 23:19:22 +0300

    got away urls from conftest

--- api_testing_practice/go_rest/conftest.py
+++ api_testing_practice/go_rest/conftest.py
@@ -2,6 +2,9 @@ import pytest
 import requests
 from api_testing_practice.go_rest.test_data.headers import headers
 from api_testing_practice.go_rest.test_data.payloads import post_payload, patch_payload
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 
 @pytest.fixture()
@@ -18,12 +21,12 @@ def update_payload():
 
 @pytest.fixture(scope="function")
 def user_id(authorization, payload):
-    response = requests.post('https://gorest.co.in/public/v2/users', headers=authorization, json=payload).json()
+    response = requests.post(f'{os.getenv('GOREST_BASE_URL')}', headers=authorization, json=payload).json()
     yield response['id']
-    requests.delete(f'https://gorest.co.in/public/v2/users/{response["id"]}', headers=authorization)
+    requests.delete(f'{os.getenv('GOREST_BASE_URL')}/{response["id"]}', headers=authorization)
 
 @pytest.fixture(scope="function")
 def user_id_for_update_user(authorization, update_payload):
-    response = requests.post('https://gorest.co.in/public/v2/users', headers=authorization, json=update_payload).json()
+    response = requests.post(f'{os.getenv('GOREST_BASE_URL')}', headers=authorization, json=update_payload).json()
     yield response['id']
-    requests.delete(f'https://gorest.co.in/public/v2/users/{response["id"]}', headers=authorization)
+    requests.delete(f'{os.getenv('GOREST_BASE_URL')}/{response["id"]}', headers=authorization)
